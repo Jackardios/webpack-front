2ca76ca2e5b44ee14d27b1f53eecd7581a990891
Jackardios <salahutdinov.salavat@gmail.com>
2018-02-27 20:28:47 +0300

    Change index.js file, add requestAnimationFrame polyfill, remove jquery from package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+// global style
 import "./index.scss";
 
-window.onload = function() {
-    svgxuse;
-};
+// polyfills
+import "babel-polyfill";
+// import requestanimationframe from './polyfills/requestanimationframe';
+
+// helpers
+// import smoothScrollingToHashInit from './helpers/smoothScrollingToHash';
+
+// components
+// import TextField from './components/TextField/TextField';
+// import Popup from './components/Popup/Popup';
+
+// blocks
+// import headerInit from './blocks/header/header';
+
+// function popupInit() {
+//     if ( !window.popupObj ) {
+//         window.popupObj = new Popup();
+//     }
+//     window.popupObj.init();
+// }
+
+$(() => {
+    // popupInit();
+    // smoothScrollingToHashInit();
+    // new TextField(".input--text");
+});
