868d12f95ebc5d677662a7da0b3b586df09c87c3
Jackardios <salahutdinov.salavat@gmail.com>
2019-04-09 00:42:31 +0300

    Add image-webpack-loader and .prettierrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const postcssNormalize = require("postcss-normalize");
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const postcssNormalize = require('postcss-normalize');
 
 const PATHS = {
-  src: path.resolve(__dirname, "src"),
-  build: path.resolve(__dirname, "build")
+  src: path.resolve(__dirname, 'src'),
+  build: path.resolve(__dirname, 'build'),
 };
 
 module.exports = (env, options) => {
-  const isDevMode = options.mode !== "production";
+  const isDevMode = options.mode !== 'production';
 
   const commonConfig = {
-    entry: path.join(PATHS.src, "scripts/app.js"),
+    entry: path.join(PATHS.src, 'scripts/app.js'),
     output: {
       path: PATHS.build,
-      filename: "app" + (isDevMode ? ".js" : ".min.js")
+      filename: 'app' + (isDevMode ? '.js' : '.min.js'),
     },
     module: {
       rules: [
@@ -28,50 +28,50 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
+            loader: 'babel-loader',
+          },
         },
         {
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|s?[ca]ss)$/,
           include: PATHS.src,
-          loader: "import-glob"
+          loader: 'import-glob',
         },
         {
           test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
           include: PATHS.src,
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
             limit: 4096,
-            name: "[path][name].[ext]"
-          }
+            name: '[path][name].[ext]',
+          },
         },
 
         {
           test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
           include: /node_modules/,
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
             limit: 4096,
-            outputPath: "vendor/",
-            name: "[name].[ext]"
-          }
+            outputPath: 'vendor/',
+            name: '[name].[ext]',
+          },
         },
         {
           test: /\.html$/,
           use: [
             {
-              loader: "html-loader",
-              options: { minimize: !isDevMode }
-            }
-          ]
-        }
-      ]
+              loader: 'html-loader',
+              options: { minimize: !isDevMode },
+            },
+          ],
+        },
+      ],
     },
     plugins: [new CleanWebpackPlugin()],
     externals: {
-      jquery: "jQuery"
-    }
+      jquery: 'jQuery',
+    },
   };
 
   const devConfig = {
@@ -79,23 +79,23 @@ module.exports = (env, options) => {
       rules: [
         {
           test: /\.(sa|sc|c)ss$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
-        }
-      ]
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new HtmlWebPackPlugin({
-        filename: "index.html",
-        template: path.join(PATHS.src, "index.html")
-      })
+        filename: 'index.html',
+        template: path.join(PATHS.src, 'index.html'),
+      }),
     ],
-    devtool: "source-map",
+    devtool: 'source-map',
     devServer: {
       hot: true,
       compress: true,
-      port: 9000
-    }
+      port: 9000,
+    },
   };
 
   const productionConfig = {
@@ -105,68 +105,104 @@ module.exports = (env, options) => {
           test: /\.(sa|sc|c)ss$/,
           use: [
             MiniCssExtractPlugin.loader,
-            "css-loader",
+            'css-loader',
             {
-              loader: "postcss-loader",
+              loader: 'postcss-loader',
               options: {
                 // Necessary for external CSS imports to work
                 // https://github.com/facebook/create-react-app/issues/2677
-                ident: "postcss",
+                ident: 'postcss',
                 plugins: () => [
-                  require("postcss-flexbugs-fixes"),
-                  require("postcss-preset-env")({
+                  require('postcss-flexbugs-fixes'),
+                  require('postcss-preset-env')({
                     autoprefixer: {
-                      flexbox: "no-2009"
+                      flexbox: 'no-2009',
                     },
-                    stage: 3
+                    stage: 3,
                   }),
                   // Adds PostCSS Normalize as the reset css with default options,
                   // so that it honors browserslist config in package.json
                   // which in turn let's users customize the target behavior as per their needs.
-                  postcssNormalize()
+                  postcssNormalize(),
                 ],
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
-            "sass-loader"
-          ]
-        }
-      ]
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                  quality: 75,
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: '75-90',
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                // the webp option will enable WEBP
+                webp: {
+                  quality: 75,
+                },
+              },
+            },
+          ],
+        },
+      ],
     },
     optimization: {
-      minimizer: [new OptimizeCSSAssetsPlugin({})],
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              warnings: true,
+              drop_console: true,
+            },
+            output: {
+              comments: false,
+            },
+          },
+          cache: true,
+          parallel: true,
+          sourceMap: true,
+        }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessor: require('cssnano'),
+          cssProcessorPluginOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }],
+          },
+        }),
+      ],
       splitChunks: {
         cacheGroups: {
           styles: {
-            name: "styles",
+            name: 'styles',
             test: /\.css$/,
-            chunks: "all",
-            enforce: true
-          }
-        }
+            chunks: 'all',
+            enforce: true,
+          },
+        },
         // chunks: "all"
-      }
+      },
     },
     plugins: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            warnings: true,
-            drop_console: true
-          },
-          output: {
-            comments: false
-          }
-        },
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
-
       new MiniCssExtractPlugin({
-        filename: "app" + (isDevMode ? ".css" : ".min.css")
-      })
-    ]
+        filename: 'app' + (isDevMode ? '.css' : '.min.css'),
+      }),
+    ],
   };
 
   return isDevMode
